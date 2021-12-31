c6da77541d565e82750d75dfdbd7633414bbb93a
yaesolKim <yaesol91@gmail.com>
2021-12-31 18:43:48 +0900

    close #8

---
+++ pick_place/src/pick_place_no_collision.cpp
@@ -0,0 +1,136 @@
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "move_group_interface");
+  ros::NodeHandle n;
+  
+  // ROS spinning must be running for the MoveGroupInterface to get information
+  // about the robot's state. One way to do this is to start an AsyncSpinner
+  // beforehand.
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+    // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
+  // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
+  // are used interchangably.
+    static const std::string PLANNING_GROUP_ARM = "left_arm";
+    static const std::string PLANNING_GROUP_GRIPPER = "left_gripper";
+    
+    // The :planning_interface:`MoveGroupInterface` class can be easily
+    // setup using just the name of the planning group you would like to control and plan for.
+    moveit::planning_interface::MoveGroupInterface move_group_interface_arm(PLANNING_GROUP_ARM);
+    moveit::planning_interface::MoveGroupInterface move_group_interface_gripper(PLANNING_GROUP_GRIPPER);
+
+    // We can get a list of all the groups in the robot:
+    ROS_INFO_NAMED("tutorial", "Available Planning Groups:");
+    std::copy(move_group_interface_arm.getJointModelGroupNames().begin(),
+    move_group_interface_arm.getJointModelGroupNames().end(), std::ostream_iterator<std::string>(std::cout, ", "));
+
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
+    
+    // 1. Move to home position
+    move_group_interface_arm.setJointValueTarget(move_group_interface_arm.getNamedTargetValues("left_arm_zero"));
+    
+    bool success = (move_group_interface_arm.plan(my_plan_arm) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_arm.move();
+
+
+    // 2. Place the TCP (Tool Center Point, the tip of the robot) above the blue box
+    geometry_msgs::PoseStamped current_pose;
+    current_pose = move_group_interface_arm.getCurrentPose("left_ee_link");
+
+    geometry_msgs::Pose box_pose;
+    box_pose.orientation.x = 0;
+    box_pose.orientation.y = 0;
+    box_pose.orientation.z = 0;
+    box_pose.orientation.w = 1;
+    box_pose.position.x = -0.725284;
+    box_pose.position.y = 0.672766;
+    box_pose.position.z = 1.013987;
+
+    geometry_msgs::Pose target_pose1; //box pose
+  
+    std::cout << "current position: " << current_pose.pose.position.x << ", " << current_pose.pose.position.y << ", " <<current_pose.pose.position.z << std::endl;
+
+    target_pose1.orientation = current_pose.pose.orientation;
+    target_pose1.position.x = box_pose.position.x;
+    target_pose1.position.y = box_pose.position.y;
+    target_pose1.position.z = box_pose.position.z + 0.5;
+
+    move_group_interface_arm.setPoseTarget(target_pose1);
+
+    success = (move_group_interface_arm.plan(my_plan_arm) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_arm.move();
+    /*
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan_gripper;
+
+    // 3. Open the gripper
+    move_group_interface_gripper.setJointValueTarget(move_group_interface_gripper.getNamedTargetValues("open"));
+
+    success = (move_group_interface_gripper.plan(my_plan_gripper) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_gripper.move();
+
+    // 4. Move the TCP close to the object
+    target_pose1.position.z = target_pose1.position.z - 0.2;
+    move_group_interface_arm.setPoseTarget(target_pose1);
+
+    success = (move_group_interface_arm.plan(my_plan_arm) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_arm.move();
+
+    // 5. Close the  gripper
+    move_group_interface_gripper.setJointValueTarget(move_group_interface_gripper.getNamedTargetValues("closed"));
+
+    success = (move_group_interface_gripper.plan(my_plan_gripper) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_gripper.move();
+
+    // 6. Move the TCP above the plate
+    target_pose1.position.z = target_pose1.position.z + 0.2;
+    target_pose1.position.x = target_pose1.position.x - 0.6;
+    move_group_interface_arm.setPoseTarget(target_pose1);
+
+    success = (move_group_interface_arm.plan(my_plan_arm) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_arm.move();
+
+    // 7. Lower the TCP above the plate
+    target_pose1.position.z = target_pose1.position.z - 0.14;
+    move_group_interface_arm.setPoseTarget(target_pose1);
+
+    success = (move_group_interface_arm.plan(my_plan_arm) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_arm.move();
+
+    // 8. Open the gripper
+    move_group_interface_gripper.setJointValueTarget(move_group_interface_gripper.getNamedTargetValues("open"));
+
+    success = (move_group_interface_gripper.plan(my_plan_gripper) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+    move_group_interface_gripper.move();
+*/
+  ros::shutdown();
+  return 0;
+}

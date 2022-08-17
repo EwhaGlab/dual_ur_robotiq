7c7e5049e45712d4453282085eddfb27f97d2b45
daeunSong <daeun7250@gmail.com>
2022-08-17 20:06:52 +0900

    Update camera pose estimate working code

--- bimanual_manipulation/src/camera_pose_estimate.cpp
+++ bimanual_manipulation/src/camera_pose_estimate.cpp
@@ -1,11 +1,14 @@
+// 0. initialization
 // 1. grasp the marker blocks
-// 2. move the block and track the marker
-// 3. move the block again and track the marker
+// 2. move the block and track the marker (P1)
+// 3. move the block again and track the marker (P2)
 // 4. calculate the world-camera transformation matrix
 // 5. save the transformation matrix as txt file
 
+
 // ??. locate the camera using the publisher like below
 // rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 1 /world /camera_link
+
 
 #include <ros/ros.h>
 #include <std_msgs/Char.h>
@@ -31,11 +34,11 @@
 shape_msgs::SolidPrimitive setPrim(int d, float x, float y, float z);
 geometry_msgs::Pose setGeomPose(float x, float y, float z, float ox, float oy, float oz, float ow);
 
-// TODO: 4. calculate camera-robot transformation
+// TODO: calculate camera-robot transformation matrix
 void arMarkersCallback(ar_track_alvar_msgs::AlvarMarkers req)
 {
     if (!req.markers.empty()) {
-      //tf::Quaternion q(req.markers[0].pose.pose.position.x, req.markers[0].pose.pose.position.y, req.markers[0].pose.pose.position.z);
+//      tf::Quaternion q(req.markers[0].pose.pose.position.x, req.markers[0].pose.pose.position.y, req.markers[0].pose.pose.position.z);
       tf::Quaternion q(req.markers[1].pose.pose.orientation.x, req.markers[1].pose.pose.orientation.y, req.markers[1].pose.pose.orientation.z, req.markers[1].pose.pose.orientation.w);
       tf::Matrix3x3 m(q);
       double roll, pitch, yaw;
@@ -45,7 +48,7 @@ void arMarkersCallback(ar_track_alvar_msgs::AlvarMarkers req)
       // roll  --> rotate around vertical axis
       // pitch --> rotate around horizontal axis
       // yaw   --> rotate around depth axis
-    }
+    } // if
 }
 
 int main(int argc, char** argv)
@@ -77,9 +80,8 @@ int main(int argc, char** argv)
   moveit_visual_tools::MoveItVisualTools visual_tools(rightArm.getPlanningFrame().c_str());
   visual_tools.deleteAllMarkers();
   visual_tools.loadRemoteControl();
-  
-  //TODO: 4. calculate camera-robot transformation
-  //ros::Subscriber sub_marker = n.subscribe("/ar_pose_marker", 100, arMarkersCallback);
+
+//  ros::Subscriber sub_marker = n.subscribe("/ar_pose_marker", 100, arMarkersCallback);
 
   // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
   Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
@@ -99,6 +101,7 @@ int main(int argc, char** argv)
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
   //activate left and right grippers
+
   gripper_msg_l.data = 'r'; // reset
   gripper_msg_r.data = 'r'; // reset
   gripper_pub_left.publish(gripper_msg_l);
@@ -119,11 +122,8 @@ int main(int argc, char** argv)
   gripper_pub_left.publish(gripper_msg_l);
   gripper_pub_right.publish(gripper_msg_r);
 
-  //0. test motion
-  geometry_msgs::PoseStamped current_pose_left = leftArm.getCurrentPose("left_gripper_tool0");
-  /*std::cout << std::endl <<"left pos: " << current_pose_left.pose.position.x << ", " << current_pose_left.pose.position.y << ", " << current_pose_left.pose.position.z << std::endl;
-  std::cout <<"ori: " << current_pose_left.pose.orientation.x << ", " << current_pose_left.pose.orientation.y << ", " << current_pose_left.pose.orientation.z << ", " << current_pose_left.pose.orientation.w << std::endl;
-  */
+  // 0-1. test motion : move the right gripper up 1 cm
+//  geometry_msgs::PoseStamped current_pose_left = leftArm.getCurrentPose("left_gripper_tool0");
   geometry_msgs::PoseStamped current_pose_right = rightArm.getCurrentPose("right_gripper_tool0");
   std::cout << std::endl <<"right pos: " << current_pose_right.pose.position.x << ", " << current_pose_right.pose.position.y << ", " << current_pose_right.pose.position.z << std::endl;
   std::cout <<"ori: " << current_pose_right.pose.orientation.x << ", " << current_pose_right.pose.orientation.y << ", " << current_pose_right.pose.orientation.z << ", " << current_pose_right.pose.orientation.w << std::endl;
@@ -131,17 +131,6 @@ int main(int argc, char** argv)
   geometry_msgs::Pose start_pose_left = current_pose_left.pose;
   geometry_msgs::Pose start_pose_right = current_pose_right.pose;
 
-/*
-  start_pose_left.position.z += 0.01;
-  leftArm.setPoseTarget(start_pose_left);
-  success = (leftArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-
-  visual_tools.publishText(text_pose, "move test", rvt::WHITE, rvt::XLARGE);
-  visual_tools.trigger();
-  visual_tools.prompt("Press 'next' to test the left arm move");
-  leftArm.move();
-*/
   start_pose_right.position.z += 0.01;
   rightArm.setPoseTarget(start_pose_right);
   success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
@@ -149,10 +138,10 @@ int main(int argc, char** argv)
 
   visual_tools.publishText(text_pose, "move test", rvt::WHITE, rvt::XLARGE);
   visual_tools.trigger();
-  visual_tools.prompt("Press 'next' to test the right arm move");
+  visual_tools.prompt("0-1. Press 'next' to test the right arm move");
   rightArm.move();
 
-  // 1-1. Add Collision object
+  // 0-2. Add Collision object
   ROS_INFO_NAMED("tutorial", "Add objects into the world");
   std::vector<moveit_msgs::CollisionObject> collision_objects;
 
@@ -191,205 +180,158 @@ int main(int argc, char** argv)
 
   visual_tools.publishText(text_pose, "Add objects", rvt::WHITE, rvt::XLARGE);
   visual_tools.trigger();
-  visual_tools.prompt("1. Press 'next' in the RvizVisualToolsGui window to once the table appears in RViz");
+  visual_tools.prompt("0-2. Press 'next' in the RvizVisualToolsGui window to once the table appears in RViz");
 
   right_goal_pose = right_block_pose;
   left_goal_pose = left_block_pose;
 
-  // 1-1. grasp the right marker block
+  // 0-3. initialize the arm
+  rightArm.setStartStateToCurrentState();
+  rightArm.setJointValueTarget("right_shoulder_pan_joint", -0.26179); //-15
+  rightArm.setJointValueTarget("right_shoulder_lift_joint", -1.3962634); //-80
+  rightArm.setJointValueTarget("right_elbow_joint", 1.91986); //110
+  rightArm.setJointValueTarget("right_wrist_1_joint", -1.3962634); //-80
+  rightArm.setJointValueTarget("right_wrist_2_joint", 2.35619); //135
+  rightArm.setJointValueTarget("right_wrist_3_joint", -0.523599);  //-30
+  success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("tutorial", "Visualizing initialization %s", success ? "" : "FAILED");
+  visual_tools.trigger();
+  visual_tools.prompt("0-3. Press 'next' once the plan is complete. Right arm moving to the initial pose);
+  rightArm.execute(my_plan);
+
+  // 1. grasp the right marker block
+  // 1-1. Move to the grasping-ready pose
   right_goal_pose.position.z += 0.1;
   rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
   success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
   visual_tools.trigger();
-  visual_tools.prompt("2-1. Press 'next' once the plan is complete and then it will move the arm");
+  visual_tools.prompt("1-1. Press 'next' once the plan is complete. Right arm moving to the grasping pose");
   rightArm.move();
 
+  // 1-2. Move down to grasping pose
   right_goal_pose.position.z -= 0.04;
   rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
   success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   visual_tools.trigger();
-  visual_tools.prompt("2-2. Press 'next' to move the right arm close to the block");
+  visual_tools.prompt("1-2. Press 'next' once the plan is complete. Right arm moving down");
   rightArm.execute(my_plan);
 
+  // 1-3. Close the gripper
   visual_tools.trigger();
-  visual_tools.prompt("3. Press 'next' to close the right gripper");
+  visual_tools.prompt("1-3. Press 'next' to close the right gripper");
   gripper_msg_r.data = 'c';
   gripper_pub_right.publish(gripper_msg_r);
 
-/*
-  // 1-2. grasp the left marker block
-  left_goal_pose.position.z += 0.1;
-  leftArm.setApproximateJointValueTarget(left_goal_pose, "left_gripper_tool0");
-  success = (leftArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-  visual_tools.trigger();
-  visual_tools.prompt("4-1. Press 'next' once the plan is complete and then it will move the left arm");
-  leftArm.execute(my_plan);
-
-  left_goal_pose.position.z -= 0.04;
-  leftArm.setApproximateJointValueTarget(left_goal_pose, "left_gripper_tool0");
-  success = (leftArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-
-  visual_tools.trigger();
-  visual_tools.prompt("4-2. Press 'next' to move the left arm close to the block");
-  leftArm.execute(my_plan);
-
-  visual_tools.trigger();
-  visual_tools.prompt("5. Press 'next' to close the left gripper");
-  gripper_msg_l.data = 'c';
-  gripper_pub_left.publish(gripper_msg_l);
-*/
-  // 1-3. remove objects from the planning scene
+  // remove objects from the planning scene
   std::vector<std::string> object_ids;
   object_ids.push_back("left_block");
   object_ids.push_back("right_block");
   planning_scene_interface.removeCollisionObjects(object_ids);
 
-  // 2-1. pick up the marker blocks
+  // 1-4. pick up the marker blocks
   right_goal_pose = rightArm.getCurrentPose("right_gripper_tool0").pose;
-  right_goal_pose.position.z += 0.03;
+  right_goal_pose.position.z += 0.10;
   rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
   success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   visual_tools.trigger();
-  visual_tools.prompt("6. Press 'next' to pick up the right block");
+  visual_tools.prompt("1-4. Press 'next' to pick up the right block");
   rightArm.execute(my_plan);
-/*
-  left_goal_pose = leftArm.getCurrentPose("left_gripper_tool0").pose;
-  left_goal_pose.position.z += 0.03;
-  leftArm.setApproximateJointValueTarget(left_goal_pose, "left_gripper_tool0");
-  success = (leftArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  // 2. move the right marker block (P1)
+  // 2-1. rotate the wrist
+  rightArm.setStartStateToCurrentState();
+  rightArm.setJointValueTarget("right_wrist_2_joint", -1.04); //-60
+  success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("tutorial", "Rotating the wrist %s", success ? "" : "FAILED");
   visual_tools.trigger();
-  visual_tools.prompt("6. Press 'next' to pick up the left block");
-  leftArm.execute(my_plan);
+  visual_tools.prompt("2-1. Press 'next' to rotate the wrist");
+  rightArm.move();
 
+  // 2-2. move the right marker block to P1
+  right_goal_pose.position.x = -0.64;
+  right_goal_pose.position.y = 0.08;
+  right_goal_pose.position.z = 1.65;
 
-  // 2-2. move the left marker block
-  //planFromCurrentPose(leftArm, my_plan, visual_tools, 10.0, 10.0, 2.0, 10.0, 10.0, 10.0, 10.0);
-
-  left_goal_pose.position.x = -0.38;
-  left_goal_pose.position.y = -0.2;
-  left_goal_pose.position.z = 1.86;
-
-  left_goal_pose.orientation.x = 0.5;
-  left_goal_pose.orientation.y = -0.7071068;
-  left_goal_pose.orientation.z = 0.0;
-  left_goal_pose.orientation.w = 0.5;
-
-  left_goal_pose.orientation.x = 0.0;
-  left_goal_pose.orientation.y = 0.0;
-  left_goal_pose.orientation.z = 0.0;
-  left_goal_pose.orientation.w = 1.0;
-
-  leftArm.setApproximateJointValueTarget(left_goal_pose, "left_gripper_tool0");
-  success = (leftArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-  visual_tools.trigger();
-  visual_tools.prompt("8. Press 'next' to move left arm to the center of the robot");
-  leftArm.move();
-*/
-  // 2-3. move the right marker block
-  right_goal_pose.position.x = -0.38;
-  right_goal_pose.position.y = 0.2;
-  right_goal_pose.position.z = 1.86;
-
-  right_goal_pose.orientation.x = -0.7071068;
-  right_goal_pose.orientation.y = 0.0;
+  right_goal_pose.orientation.x = 0.0;
+  right_goal_pose.orientation.y = -0.7071068;
   right_goal_pose.orientation.z = 0.0;
   right_goal_pose.orientation.w = 0.7071068;
 
   rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
   success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  // repeat until it succeeds
+  while (!success)
+  {
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+    rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
+    success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  }
+
   ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
   visual_tools.trigger();
-  visual_tools.prompt("Press 'next' once the plan is complete and then it will move the arm");
+  visual_tools.prompt("2-2. Press 'next' once the plan is complete. Move the block to P1");
   rightArm.move();
 
-/*
- // 2. move the left marker block
-//  left_goal_pose = leftArm.getCurrentPose("left_gripper_tool0").pose;
-
-  //setGoalPose(left_goal_pose, -0.5, -0.2, 1.8, -0.5, -0.5, 0.5, 0.5);
-
-  left_goal_pose.position.x = -0.5;
-  left_goal_pose.position.y = -0.2;
-  left_goal_pose.position.z = 1.8;
-
-  left_goal_pose.orientation.x = -0.5;
-  left_goal_pose.orientation.y = -0.5;
-  left_goal_pose.orientation.z = 0.5;
-  left_goal_pose.orientation.w = 0.5;
-
-  leftArm.setApproximateJointValueTarget(left_goal_pose, "left_gripper_tool0");
-  success = (leftArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-  visual_tools.trigger();
-  visual_tools.prompt("8. Press 'next' to move left arm to the center of the robot");
-  leftArm.move();
-
-  // 8. move the right marker block
-  right_goal_pose.position.x = -0.5;
-  right_goal_pose.position.y = 0.2;
-  right_goal_pose.position.z = 1.8;
-
-  right_goal_pose.orientation.x = 0.5;
-  right_goal_pose.orientation.y = -0.5;
-  right_goal_pose.orientation.z = -0.5;
-  right_goal_pose.orientation.w = 0.5;
-
-  rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
-  success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-  visual_tools.trigger();
-
-  visual_tools.prompt("Press 'next' once the plan is complete and then it will move the arm");
-  rightArm.move();
-  */
-  //마커 하나의 rpy로 일단 각도 먼저 잡는다.
 
   //TODO: subscribe marker pose
-  //TODO: 4. calculate camera to robot base
-  //TODO: 5. save the transformation to txt file
 
-/*
-  // 2. move the left marker block
-  left_goal_pose.position.x = -0.5;
-  left_goal_pose.position.y = -0.2;
-  left_goal_pose.position.z = 1.65;
 
-  left_goal_pose.orientation.x = -0.5;
-  left_goal_pose.orientation.y = -0.5;
-  left_goal_pose.orientation.z = 0.5;
-  left_goal_pose.orientation.w = 0.5;
-
-  leftArm.setApproximateJointValueTarget(left_goal_pose, "left_gripper_tool0");
-  success = (leftArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-  visual_tools.trigger();
-  visual_tools.prompt("8. Press 'next' to move left arm to the center of the robot");
-  leftArm.move();
-*/
-  // 8. move the right marker block
-  right_goal_pose.position.x = -0.5;
+  // 3. move the right marker block (P2)
+  right_goal_pose.position.x = -0.38;
   right_goal_pose.position.y = 0.2;
-  right_goal_pose.position.z = 1.65;
+  right_goal_pose.position.z = 1.86;
 
-  right_goal_pose.orientation.x = 0.5;
-  right_goal_pose.orientation.y = -0.5;
-  right_goal_pose.orientation.z = -0.5;
-  right_goal_pose.orientation.w = 0.5;
+  right_goal_pose.orientation.x = 0.5926;
+  right_goal_pose.orientation.y = -0.3856;
+  right_goal_pose.orientation.z = -0.3856;
+  right_goal_pose.orientation.w = 0.5926;
 
   rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
   success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  // repeat until it succeeds
+  while (!success)
+  {
+    ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+    rightArm.setApproximateJointValueTarget(right_goal_pose, "right_gripper_tool0");
+    success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  }
+
   ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
   visual_tools.trigger();
-  visual_tools.prompt("Press 'next' once the plan is complete and then it will move the arm");
+  visual_tools.prompt("3. Press 'next' once the plan is complete. Move the block to P2");
   rightArm.move();
 
-  // TODO: 4. calculate the world-camera transformation matrix -> from the subscriber callback function
-    
-  // TODO: 5. save the transformation as txt file
-    
+  //TODO: subscribe marker pose
+
+
+  // FINAL. move to initial pose
+  rightArm.setStartStateToCurrentState();
+  rightArm.setJointValueTarget("right_shoulder_pan_joint", -0.26179); //-15
+  rightArm.setJointValueTarget("right_shoulder_lift_joint", -1.3962634); //-80
+  rightArm.setJointValueTarget("right_elbow_joint", 1.91986); //110
+  rightArm.setJointValueTarget("right_wrist_1_joint", -1.3962634); //-80
+  rightArm.setJointValueTarget("right_wrist_2_joint", 2.35619); //135
+  rightArm.setJointValueTarget("right_wrist_3_joint", -0.523599);  //-30
+  success = (rightArm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("tutorial", "Visualizing initialization %s", success ? "" : "FAILED");
+
+  visual_tools.publishText(text_pose, "move test", rvt::WHITE, rvt::XLARGE);
+  visual_tools.trigger();
+  visual_tools.prompt("Press 'next' to test the right arm move");
+  rightArm.execute(my_plan);
+
+  visual_tools.trigger();
+  visual_tools.prompt("Press 'next' to open the right gripper");
+  gripper_msg_r.data = 'o';
+  gripper_pub_right.publish(gripper_msg_r);
+
+
+  //TODO: calculate camera to robot base transformation
+  //TODO: save the transformation to txt file
+
   file << "";
   file.close();
 
